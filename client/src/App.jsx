bfd67aa0e5632590b441e5a2c8e0448fca531cf6
vigneshxcode1 <vigneshxcode@gmail.com>
2025-01-21 19:48:13 +0530

    2 commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './Compoenent/Navbar/Navbar'
 import Setup from './Compoenent/Setup/Setup'
 import Banner from './Compoenent/banner/Banner'
 import Philosophy from './Compoenent/philosophy/Philosophy'
-import TestimonialCarousel from './Compoenent/testimonial/testimonial'
+import TestimonialCarousel from './Compoenent/testimonial/testimonial.jsx'
 import PromoSection from './Compoenent/footer/Footer'
 
 function App() {
